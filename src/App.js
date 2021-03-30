5ef0d9bcc1cf717eba9b2243cf9929c3f3ca2ebc
hautera <hautea@uw.edu>
2021-03-29 21:37:11 -0700

    Click

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect, useRef, useState } from 'react';
 
+
 //returns the unit vector turned 60 * a degrees where a is an integer 
 //returns null if a < 0
 const num_60rotations = (a) => {
@@ -10,6 +11,19 @@ const num_60rotations = (a) => {
     return [0.5, 0.866];
   } else if (a === 2) {
     return [-0.5, 0.866];
+  } else if (a >= 3) {
+    let [al,be] = num_60rotations(a - 3);
+    return [-al, -be]; //beautiful 
+  } 
+}
+
+const num_60rotationsOffset30 = (a) => {
+  if (a === 0) {
+    return [0.866,0.5];
+  } else if (a === 1) {
+    return [0, 1];
+  } else if (a === 2) {
+    return [-0.866, 0.5];
   } else if (a >= 3) {
     let [al,be] = num_60rotations(a - 3);
     return [-al, -be]; //beautiful 
@@ -39,12 +53,6 @@ class Hex {
     return dx ** 2 + dy ** 2 
   }
 
-  //returns true if x,y are in the hexagon
-  //TODO make this work 
-  contains(x,y) {
-    return false;
-  }
-
   //given the 3-coordinate hex grid and a hexagon size,
   //calculate the center of this hexagon
   center() {
@@ -59,16 +67,16 @@ class Hex {
   }
 
   //draws this hexagon on the screen 
-  draw = (size, context, color, x_offset = 0, y_offset = 0) => {
+  draw = (size, context, color, x_offset = 0, y_offset = 0, fill = false) => {
     context.fillStyle = color;
     let [center_x , center_y] = this.center();
     context.beginPath();
     for(let i = 0; i < 7; i++){
-      let [x0, y0] = num_60rotations(i);
+      let [x0, y0] = num_60rotations(i+2);
       context.lineTo(size * (x0 + center_x) + x_offset, size * (y0 + center_y) + y_offset);
     }
     context.closePath();
-    context.stroke();
+    if( fill ) { context.fill() } else { context.stroke(); }
     context.fillText(this.toString(), center_x * size + x_offset, center_y * size + y_offset, size)
   };
 
@@ -85,7 +93,8 @@ class Hex {
 
 class Map {
   //size represents the number of layers we will move from the center
-  constructor (size, center_x, center_y) {
+  constructor (size, center_x, center_y, hexSize) {
+    this.size = hexSize;
     this.center_x = center_x;
     this.center_y = center_y;
     this.hexes = new Set();
@@ -103,24 +112,75 @@ class Map {
     this.center_y = y;
   }
 
-  draw(hexSize, context, drawAxis=false) {
+  setSize(newSize) {
+    this.size = newSize;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  draw(context, drawAxis=false) {
+    const hexSize = this.size;
     this.hexes.forEach( (hex) => hex.draw(hexSize, context, 'white', this.center_x, this.center_y) );
     if (drawAxis) {
-      const axis_rot = [0,2,4];
-      const axis_name = ['x', 'y', 'z'];
+      const axis_rot = [0, 2, 4];
+      //const axis_name = ['x', 'y', 'z'];
       axis_rot.forEach( (a, index) => {
         const [x, y] = num_60rotations(a);
         context.beginPath();
         context.moveTo(this.center_x, this.center_y);
         context.lineWidth = 5; 
         context.strokeStyle = 'red';
-        context.lineTo(this.center_x - x * 500, this.center_y - y * 500);
-        context.fillText(axis_name[index], this.center_x - x * 500, this.center_y - y * 500);
-        //context.lineTo(center_x + x * 500, center_y + y * 500);
+        //context.lineTo(this.center_x - x * 500, this.center_y - y * 500);
+        context.lineTo(this.center_x + x * 500, this.center_y + y * 500);
         context.stroke();
         context.closePath();
       });
     }
+  }
+
+  findHex(x_offset, y_offset) {
+    //the map seems to be drawn 28 pixels too high :/
+    //console.log(x_offset, y_offset);
+    //console.log(x_offset - this.center_x, y_offset - this.center_y + 28)
+    
+    let euc_x = (x_offset - this.center_x)/(1.5*this.size);
+    let euc_y = (y_offset - this.center_y)/(1.5*this.size);
+
+    const x = euc_x * num_60rotations(0)[0] + euc_y * num_60rotations(0)[1];
+    const y = euc_x * num_60rotations(2)[0] + euc_y * num_60rotations(2)[1];
+    const z = euc_x * num_60rotations(4)[0] + euc_y * num_60rotations(4)[1];
+    const rx = Math.round(x), ry = Math.round(y), rz = Math.round(z);
+    // coordinates must add to zero 
+    if ( rx + ry + rz === 0) { return [rx, ry, rz]; }
+    // if we make it this far, there is a rounding error,
+    // we fix by finding nearest solution 
+    let c = (rx + ry + rz >= 0) ? -1 : 1;
+    let minusX = this.distance(x,y,z, rx + c, ry, rz);
+    let minusY = this.distance(x,y,z, rx, ry + c, rz);
+    let minusZ = this.distance(x,y,z, rx, ry, rz + c);
+    const minDistance = Math.min(minusX, minusY, minusZ);
+    if (minDistance === minusX) { return [rx + c, ry, rz]; }
+    if (minDistance === minusY) { return [rx, ry + c, rz]; }
+    if (minDistance === minusZ) { return [rx, ry, rz + c]; }
+    
+    return [null, null, null];
+  }
+
+  distance(x, y, z, rx, ry, rz) {
+    return Math.abs(x - rx) + Math.abs(y - ry) + Math.abs(z - rz);
+  }
+
+  selectHex(x, y, z) {
+    for(let hex of this.hexes.values()) {
+      const [_x, _y, _z] = hex.getCoords();
+      //console.log(_x,_y,_z);
+      if(x === _x && y === _y && z === _z) {
+        return hex;
+      }
+    }
+    return null;
   }
 
   //returns a list of hexagons adjacent to the hexagon passed
@@ -129,12 +189,28 @@ class Map {
   }
 }
 
+const colorDict = (x,y,z) => {
+  if ( x === 0  && y === 0 && z === 0 ) {return 'white';}
+  if ( x === 0 && y === -1 && z === 1 ) {return 'red';}
+  if ( x === -1 && y === 0 && z === 1 ) {return 'yellow';}
+  if ( x === -1 && y === 1 && z === 0 ) {return 'orange';}
+  if ( x === 1 && y === -1 && z === 0 ) {return 'orange';}
+  if ( x === 0 && y === 1 && z === -1 ) {return 'red';}
+  if ( x === 1 && y === 0 && z === -1 ) {return 'yellow';}
+  return 'black';
+}
+
+
+const dot = (x, y, color, context) => {
+  context.fillStyle = color;
+  context.fillRect(x,y,1,1);
+}
+
 function App() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
-  const mapRef = useRef(new Map(9, 500, 400));
+  const mapRef = useRef(new Map(5, 500, 400, 50));
   const [dragging, setDragging] = useState(false);
-  const [size, setSize] = useState(50);
 
   const clearScreen = () => {
     const canvas = canvasRef.current;
@@ -150,10 +226,10 @@ function App() {
   };
 
   const resize = (inc) => {
-    setSize(size + inc);
     clearScreen();
     const map = mapRef.current;
-    map.draw(size + inc, canvasRef.current.getContext('2d'));
+    map.setSize(map.getSize() + inc);
+    map.draw(canvasRef.current.getContext('2d'), true);
   }
 
   useEffect(() => {
@@ -161,7 +237,8 @@ function App() {
     const canvas = canvasRef.current;
     let map = mapRef.current;
     const context = canvas.getContext('2d');
-    map.draw(size, context);
+    map.draw(context, true);
+    //clickTest();
   }, []);
 
   const startDragging = () => {
@@ -179,10 +256,42 @@ function App() {
       let y_offset = event.clientY;
       const map = mapRef.current;
       map.incCenter(x_offset, y_offset);
-      map.draw(size, canvasRef.current.getContext('2d'));
+      map.draw(canvasRef.current.getContext('2d'), true);
     }
   }
 
+  const findHex = (e) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const x = e.clientX;
+    const y = e.clientY;
+    const map = mapRef.current;
+    clearScreen();
+    map.draw(ctx, true);
+    const [a,b,c] = map.findHex(x,y);
+    console.log([a,b,c])
+    const hex = map.selectHex(a,b,c);
+    hex.draw(map.size, ctx, 'red', map.center_x, map.center_y, true);
+  }
+
+  //used for debugging 
+  //makes a cool montecarlo picture tho
+  const clickTest = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const maxX = canvas.width;
+    const maxY = canvas.height;
+    const map = mapRef.current;
+    console.log("Starting new click test");
+    for (let i = 0; i < 1000000; i ++) {
+      let rand_X = Math.random() * maxX;
+      let rand_Y = Math.random() * maxY;
+      const [x,y,z] = map.findHex(rand_X, rand_Y);
+      const color = colorDict(x,y,z);
+      //console.log(color);
+      dot(rand_X, rand_Y, color, ctx);
+    }
+  }
 
   return (
     <div className="App">
@@ -192,6 +301,7 @@ function App() {
           onMouseDown={startDragging}
           onMouseUp={stopDragging}
           onMouseMoveCapture={drag}
+          onClick={findHex}
         />
         <div id='control-panel'>
           <button onClick={ () => {resize(10)}}>+</button>
